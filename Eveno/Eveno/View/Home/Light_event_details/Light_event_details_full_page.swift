5d311ed2c100270ff9bc34ef265488ee14b8bc20
BillyZeBlack <34786459+BillyZeBlack@users.noreply.github.com>
2022-10-20 07:41:59 +0200

    Initialisation de la vue

---
+++ Eveno/Eveno/View/Home/Light_event_details/Light_event_details_full_page.swift
@@ -0,0 +1,20 @@
+//
+//  Light_event_details_full_page.swift
+//  Eveno
+//
+//  Created by williams saadi on 20/10/2022.
+//
+
+import SwiftUI
+
+struct Light_event_details_full_page: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Light_event_details_full_page_Previews: PreviewProvider {
+    static var previews: some View {
+        Light_event_details_full_page()
+    }
+}
